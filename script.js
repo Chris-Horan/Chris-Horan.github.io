afb9b34327492e1d1a521065a1af5ecccfa988be
Chris-Horan <chris.horan27@gmail.com>
2018-12-27 17:02:41 -0800

    more

--- script.js
+++ script.js
@@ -10,15 +10,15 @@ function scrollAdjust() {
     var fromTop = window.pageYOffset;
     var height = window.innerHeight;
     if(fromTop > height*0.125) {
-        textBox1.className = "hide";
+        textBox1.className = "hide disappear";
     } else textBox1.className = "show";
     if(fromTop > height*0.3 && fromTop < height*0.3+900) {
         text2.className = "show";
     } else text2.className = "hide";
-    if(fromTop > height*0.3+700 && fromTop < height*0.3+1500) {
+    if(fromTop > height*0.3+700 && fromTop < height*0.3+1800) {
         text3.className = "show";
     } else text3.className = "hide";
-    if(fromTop > height*0.3+1700 && fromTop < height*0.3+2000) {
+    if(fromTop > height*0.3+1700 && fromTop < height*0.3+2500) {
         text4.className = "show";
     } else text4.className = "hide";
 }
