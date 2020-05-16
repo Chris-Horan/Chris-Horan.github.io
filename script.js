2bd4915e2095e6d61c89467f7fff1bf75dbb9e0e
Chris-Horan <chris.horan27@gmail.com>
2020-05-16 15:32:25 -0700

    Updating Information

--- script.js
+++ script.js
@@ -12,13 +12,13 @@ function scrollAdjust() {
     if(fromTop > height*0.15) {
         textBox1.className = "hide disappear";
     } else textBox1.className = "show";
-    if(fromTop > height*0.3 && fromTop < height*0.3+900) {
+    if(fromTop > height*0.3) {
         text2.className = "show";
     } else text2.className = "hide";
-    if(fromTop > height*0.3+700 && fromTop < height*0.3+1800) {
+    if(fromTop > height*0.3+500) {
         text3.className = "show";
     } else text3.className = "hide";
-    if(fromTop > height*0.3+1600 && fromTop < height*0.3+2500) {
+    if(fromTop > height*0.3+1600) {
         block4.className = "show";
     } else block4.className = "hide";
 }
